b1987ca160767cee8db0088b88eff0139c35544b
Ubuntu <ubuntu@ip-172-31-11-226.eu-west-2.compute.internal>
2017-09-01 07:07:27 +0000

    working version

--- MetSQLForecast.py
+++ MetSQLForecast.py
@@ -27,20 +27,23 @@ ForecastList = M.loc_forecast(metoffer.CAPABILITIES,metoffer.THREE_HOURLY)
 for Forecast in ForecastList["Resource"]["TimeSteps"]["TS"]:
 	print Forecast
 	x = M.loc_forecast(metoffer.ALL,metoffer.THREE_HOURLY,Forecast)
-	publishtime = x["SiteRep"]["DV"]["data_date"]
+	publishtime = x["SiteRep"]["DV"]["dataDate"]
 	publushtime = datetime.datetime.strptime(publishtime,"%Y-%m-%dT%H:%M:%SZ")
+	forecasttime = datetime.datetime.strptime(Forecast,"%Y-%m-%dT%H:%M:%SZ")	
+	print forecasttime
+	print publushtime
 	#Now see if this is a new observation, or one we've seen before.
-	query = ("SELECT NObs FROM forecasts WHERE PublishTime = %s")
-	cursor.execute(query,(publushtime))
+	query = ("SELECT NObs FROM forecasts WHERE PublishTime = %s AND timestamp = %s")
+	cursor.execute(query,(publushtime,forecasttime))
 	NMatch = 0
 	for (NObs) in cursor:
 		NMatch += 1
-		if NMatch == 0:
-			print("Adding records for current time stamp")
-			bContinue = 1
-		else:
-			print ("Not duplicating current time stamp")
-			bContinue = 0
+	if NMatch == 0:
+		print("Adding records for current time stamp")
+		bContinue = 1
+	else:
+		print ("Not duplicating current time stamp")
+		bContinue = 0
 	
 	if bContinue:
 		x2 = x
@@ -54,7 +57,6 @@ for Forecast in ForecastList["Resource"]["TimeSteps"]["TS"]:
 
 			try:
 				y = metoffer.parse_val(x2)
-			
 			
 				if bContinue:
 					for forecast in y.data:
@@ -72,7 +74,8 @@ for Forecast in ForecastList["Resource"]["TimeSteps"]["TS"]:
 						addobs_string = ("INSERT INTO forecasts " + fieldnamestring + ") " + formatstring + ")" )
 						cursor.execute(addobs_string, tuple(obsdata_list))
 						NObs = cursor.lastrowid
-				#Commit everything for this location.
+			#Commit everything for this location
+			#	print Location["i"]
 			except:
 				print "Something went wrong somewhere, skipping this one"
 		cnx.commit()
